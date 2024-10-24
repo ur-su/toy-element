bcbfe725d400f785be39ff6df45ac9ddc3f0108b
SuYun <601971945@qq.com>
2024-10-24 16:29:41 +0800

    :tada: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,19 @@
+<!--
+ * Button.vue
+ * Button.test.vue
+ * types.ts
+ * style.css
+ * index.ts
+ * constants.ts
+-->
+<script setup lang="ts">
+defineOptions({
+  name: "ErButton",
+});
+</script>
+
+<template>
+  <button style="background-color: red; color: aliceblue">
+    this is a button
+  </button>
+</template>
